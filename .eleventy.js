30e753eb1664602c2453116feb59f64d51941eae
Isi <isabella.spanner@gmail.com>
2026-08-27 14:38:57 +0100

    Migrate experiences page to Eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,37 @@
-module.exports = function (eleventyConfig) {
+ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("src/assets");
+
   eleventyConfig.addPassthroughCopy("src/.htaccess");
+
   eleventyConfig.addPassthroughCopy("src/robots.txt");
+
   eleventyConfig.addPassthroughCopy("src/sitemap.xml");
 
+  // Bestehende .html-URLs während der Migration beibehalten
+  eleventyConfig.addGlobalData("permalink", () => {
+    return (data) =>
+      `${data.page.filePathStem}.${data.page.outputFileExtension}`;
+  });
+
   return {
+
     dir: {
+
       input: "src",
+
       includes: "_includes",
+
       output: "_site"
+
     },
+
     htmlTemplateEngine: "njk",
+
     markdownTemplateEngine: "njk",
+
     templateFormats: ["html", "njk", "md"]
+
   };
+
 };
