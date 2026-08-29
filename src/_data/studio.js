ccb9b569c6c3f82e2284ad01612066754788e242
Isi <isabella.spanner@gmail.com>
2026-08-29 18:05:05 +0100

    Add workshop event structured data

---
+++ src/_data/studio.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: "PURACERÂMICA",
+  url: "https://puraceramica.pt",
+
+  location: {
+    name: "PURACERÂMICA",
+    streetAddress: "Rua Jau 23A",
+    postalCode: "1300-311",
+    city: "Lisboa",
+    country: "PT"
+  }
+};
